50723972e26f60fab8126cd8749e4a375b87bc90
himdol <ihimchan746@gmail.com>
2023-05-14 15:03:50 +0900

    aboutMe page 이동

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '구내식당' });
+});
+
+router.get('/aboutMe', function (req, res) {
+  res.render('aboutMe', {title: '만든이'});
 });
 
 module.exports = router;
